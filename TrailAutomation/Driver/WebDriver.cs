41405bf167155273867d69c8d891b583651788e3
dubeywati <abhishek@clare.ai>
2023-08-30 08:33:00 +0530

    trail project
    adding trial project

---
+++ TrailAutomation/Driver/WebDriver.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Interactions;
+
+namespace TestAutomation
+{
+    public class WebDriver : Configurations
+    {
+        public IWebDriver Driver;
+
+        public WebDriver()
+        {
+            var chromeOptions = new ChromeOptions();
+            //chromeOptions.AddArguments("headless");
+            chromeOptions.AddArguments("window-size=1920,1080");
+            Driver = new ChromeDriver(chromeOptions);
+
+            // Maximize the browser window
+            //Driver.Manage().Window.Maximize();
+        }
+    }
+}
