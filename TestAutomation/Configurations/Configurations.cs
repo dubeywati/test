837afe3df0db7ea4a59479fedac1c0705fc21060
dubeywati <139735582+dubeywati@users.noreply.github.com>
2023-08-30 10:01:08 +0530

    Update Configurations.cs

--- TestAutomation/Configurations/Configurations.cs
+++ TestAutomation/Configurations/Configurations.cs
@@ -1,12 +1,12 @@
-﻿using System;
+using System;
 using System.Net.Http;
 using System.Text;
 using Newtonsoft.Json.Linq;
 
 namespace TestAutomation
 {
-    public class Configurations
-    {
+	public class Configurations
+	{
         string GitHubUser { get; set; }
 
         string BranchName { get; set; }
@@ -17,14 +17,14 @@ namespace TestAutomation
 
         public string JenkinsUser { get; set; }
 
-        public string Username { get; set; }
+		public string Username { get; set; }
 
         public string Password { get; set; }
 
         string JenkinsJobName { get; set; }
 
         public Configurations()
-        {
+		{
 
             /*
              * To run test cases locally set below value to false. but makesure push it to the branch 
@@ -40,60 +40,43 @@ namespace TestAutomation
 
             CheckBuildStatus();
 
-            //TriggerJenkinsJob();
+            // TriggerJenkinsJob();
 
             CheckBuildStatus();
 
         }
 
-        void LoadSeetingsForGitHubAction()
+		void LoadSeetingsForGitHubAction()
         {
 
-            GitHubUser = dubeywati;
-            BranchName = main;
+            GitHubUser = "dubeywati";
+            BranchName = "master";
             JenkinsApiToken = Environment.GetEnvironmentVariable($"{GitHubUser.ToUpper()}_JENKINS_API_TOKEN");
-            URL = Environment.GetEnvironmentVariable($"{GitHubUser.ToUpper()}_DEV_URL_TEST");
+            URL = Environment.GetEnvironmentVariable($"{GitHubUser.ToUpper()}_DEV_URL");
             JenkinsUser = Environment.GetEnvironmentVariable($"{GitHubUser.ToUpper()}_JENKINS_USER");
             Username = Environment.GetEnvironmentVariable($"{GitHubUser.ToUpper()}_DEV_USERNAME");
             Password = Environment.GetEnvironmentVariable($"{GitHubUser.ToUpper()}_DEV_PASSWORD");
             JenkinsJobName = Environment.GetEnvironmentVariable($"{GitHubUser.ToUpper()}_JENKINS_DEPLOYMENT_JOB_NAME");
         }
 
-        void LoadSeetingsForLocalRun()
+      void LoadSeetingsForLocalRun()
         {
             GitHubUser = "dubeywati";
             BranchName = "master";
             JenkinsApiToken = "111189db382717f939012be0b4b14dfa72";
-            //URL = "https://live-300005.watiapp.io/";
-            URL = "https://app.wati.io/login";
+            URL = "https://live-300005.watiapp.io/";
             JenkinsUser = "abhishek";
             Username = "abhishek@clare.ai";
             Password = "Abhishek@1";
             JenkinsJobName = "wati";
         }
-        //void LoadSeetingsForLocalRun()
-        //{
-        //    GitHubUser = "dubeywati";
-        //    BranchName = "master";
-        //    JenkinsApiToken = "111189db382717f939012be0b4b14dfa72";
-        //    URL = "https://live-104103.watiapp.io";
-        //    JenkinsUser = "abhishek";
-        //    Username = "aaditya@clare.ai";
-        //    Password = "aA9SEk6tYj&ihBRcxOZ6";
-        //    JenkinsJobName = "wati";
-        //}
 
-
-
-        
-            
-           
 
         public void TriggerJenkinsJob()
         {
             using (var httpClient = new HttpClient())
             {
-                using (var request = new HttpRequestMessage(new HttpMethod("POST"), $"http://localhost:8080/job/{JenkinsJobName}/buildWithParameters?token=apiToken"))
+                using (var request = new HttpRequestMessage(new HttpMethod("POST"), $"https://ci.clare.ai/job/{JenkinsJobName}/buildWithParameters?token=apiToken"))
                 {
                     var base64authorization = Convert.ToBase64String(Encoding.ASCII.GetBytes($"{JenkinsUser}:{JenkinsApiToken}"));
                     request.Headers.TryAddWithoutValidation("Authorization", $"Basic {base64authorization}");
@@ -107,51 +90,19 @@ namespace TestAutomation
             }
         }
 
-        //public void TriggerJenkinsJob()
-        //{
-        //    using (var httpClient = new HttpClient())
-        //    {
-        //        try
-        //        {
-        //            var jenkinsUrl = "http://localhost:8080"; // Replace with the remote Jenkins server URL
-        //            var apiEndpoint = $"{jenkinsUrl}/job/{JenkinsJobName}/buildWithParameters?token=apiToken";
-
-        //            var base64authorization = Convert.ToBase64String(Encoding.ASCII.GetBytes($"{JenkinsUser}:{JenkinsApiToken}"));
-        //            httpClient.DefaultRequestHeaders.TryAddWithoutValidation("Authorization", $"Basic {base64authorization}");
-
-        //            var multipartContent = new MultipartFormDataContent();
-        //            multipartContent.Add(new StringContent($"{BranchName}"), "BUILD_BRANCH");
-        //            var response = httpClient.PostAsync(apiEndpoint, multipartContent).GetAwaiter().GetResult();
-
-        //            if (response.IsSuccessStatusCode)
-        //            {
-        //                Console.WriteLine("Jenkins job triggered successfully.");
-        //            }
-        //            else
-        //            {
-        //                Console.WriteLine($"Failed to trigger Jenkins job. Status code: {response.StatusCode}");
-        //            }
-        //        }
-        //        catch (Exception ex)
-        //        {
-        //            Console.WriteLine($"An error occurred: {ex.Message}");
-        //        }
-        //    }
-        //}
-
         public void CheckBuildStatus()
         {
             Task.Delay(30000).Wait();
             using (var httpClient = new HttpClient())
             {
-                using (var request = new HttpRequestMessage(new HttpMethod("GET"), $"http://localhost:8080/job/{JenkinsJobName}/lastBuild/api/json"))
+                using (var request = new HttpRequestMessage(new HttpMethod("GET"), $"https://ci.clare.ai/job/{JenkinsJobName}/lastBuild/api/json"))
                 {
                     var base64authorization = Convert.ToBase64String(Encoding.ASCII.GetBytes($"{JenkinsUser}:{JenkinsApiToken}"));
                     request.Headers.TryAddWithoutValidation("Authorization", $"Basic {base64authorization}");
 
                     var response = httpClient.Send(request);
                     var jsonResult = JObject.Parse(response.Content.ReadAsStringAsync().Result ?? "");
-                    var status = jsonResult["result"]?.ToString();
+                    var status = jsonResult["result"].ToString();
                     if (status == "")
                     {
                         Task.Delay(30000).Wait();
@@ -165,9 +116,3 @@ namespace TestAutomation
 
     }
 }
-
-
-
-
-
-
