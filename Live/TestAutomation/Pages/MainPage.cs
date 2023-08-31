22f1da54ee7b1cfc667fdba965dc52f559a75137
dubeywati <139735582+dubeywati@users.noreply.github.com>
2023-08-31 17:31:14 +0530

    Update MainPage.cs

--- Live/TestAutomation/Pages/MainPage.cs
+++ Live/TestAutomation/Pages/MainPage.cs
@@ -20,8 +20,8 @@ namespace TestAutomation
         public void VerifyLogin()
         {
             var messageButton = webDriver.Driver.FindElement(By.CssSelector("[data-testid='teamInbox-leftSide-actionBar-newMessage-button']"));
-            Assert.That(messageButton.Displayed, Is.True);
-             WebDriverWait wait = new WebDriverWait(webDriver.Driver, TimeSpan.FromSeconds(15));
+            // Assert.That(messageButton.Displayed, Is.True);
+             // WebDriverWait wait = new WebDriverWait(webDriver.Driver, TimeSpan.FromSeconds(15));
              // wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(By.CssSelector($"[class='MuiButtonBase-root MuiIconButton-root MuiIconButton-sizeMedium css-1yxmbwk']")));
            // IWebElement buttonElement = webDriver.Driver.FindElement(By.CssSelector("button.MuiButtonBase-root.MuiIconButton-root.MuiIconButton-sizeMedium.css-1yxmbwk"));
 
